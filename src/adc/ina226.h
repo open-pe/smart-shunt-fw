5f4bf1708a5b44de20b5f4c8299e0041b0988cb8
Fabian Schlieper <fabian@schlieper.email>
2023-05-04 18:08:29 +0200

    ina226 calib

--- src/adc/ina226.h
+++ src/adc/ina226.h
@@ -6,6 +6,9 @@
 
 class PowerSampler_INA226;
 
+#ifndef IRAM_ATTR
+#define IRAM_ATTR
+#endif
 void IRAM_ATTR ina225_alert();
 
 PowerSampler_INA226 *ina226_instance = nullptr;
@@ -35,17 +38,19 @@ public:
         if (!ina226.init())
             return false;
 
-        //ina226.setAverage(AVERAGE_64);
-        //ina226.setConversionTime(CONV_TIME_1100);
+        ina226.setAverage(AVERAGE_1);
+        ina226.setConversionTime(CONV_TIME_1100);
 
-        ina226.setAverage(AVERAGE_1024);
-        ina226.setConversionTime(CONV_TIME_8244);
+        //ina226.setAverage(AVERAGE_1024);
+        //ina226.setConversionTime(CONV_TIME_8244);
 
         // ^^^ Conversion ready after conversion time x number of averages x 2
 
         ina226.setMeasureMode(CONTINUOUS);
 
-        ina226.setResistorRange(50e-3f / 100, 20.0f);
+        //ina226.setResistorRange(50e-3f / 100, 20.0f);
+        //ina226.setResistorRange(75e-3f / 20, 20.0f); // 20 A shunt
+        ina226.setResistorRange(75e-3f / 40, 20.0f); // 20 A shunt
 
 
         uint8_t READY_PIN = 19;
@@ -68,7 +73,7 @@ public:
     bool hasData() {
         if(!new_data)
             return false;
-        
+
         ina226.readAndClearFlags();
         if (ina226.overflow) {
             ESP_LOGW("ina226", "Overflow!");
@@ -83,8 +88,12 @@ public:
     Sample getSample() {
 
         lastSample.setTimeNow();
-        lastSample.i = ina226.getCurrent_A() * (0.756f / 0.772f);
-        lastSample.u = ina226.getBusVoltage_V() * (44.604f / 18.321f * 31.018f / 31.011f);
+
+        float shuntCorr = 0.99619f; // 40A radiomag
+        // float shuntCorr = 1.000867f; // resistor today 0.5mOhm
+
+        lastSample.i = ina226.getCurrent_A() * shuntCorr; //
+        lastSample.u = ina226.getBusVoltage_V() * (44.604f / 18.321f * 31.018f / 31.011f * 1.0006088f);
 
         float busP = ina226.getBusPower();
         float pErr = std::abs(lastSample.p() - busP) / busP;
